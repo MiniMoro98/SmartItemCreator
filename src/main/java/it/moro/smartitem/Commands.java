58b190155a83930f577e656d0b429c123101a9d8
MiniMoro98 <moro18391@gmail.com>
2025-03-20 23:55:11 +0100

    Aggiornamenti

--- src/main/java/it/moro/smartitem/Commands.java
+++ src/main/java/it/moro/smartitem/Commands.java
@@ -1,5 +1,6 @@
 package it.moro.smartitem;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -15,53 +16,80 @@ import java.util.List;
 
 public class Commands implements CommandExecutor, TabCompleter, Listener {
     private static SmartItemCreator plugin;
+
     public Commands(SmartItemCreator plugin) {
-    Commands.plugin = plugin;
+        Commands.plugin = plugin;
     }
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
-        if (!(sender instanceof Player player)) {
-            sender.sendMessage("§cSolo i giocatori possono eseguire questo comando!");
-            return true;
-        }
-        if(player.hasPermission("smartitemcreator.cmd")) {
+        if (sender instanceof Player player) {
+            if (player.hasPermission("smartitemcreator.cmd")) {
+                if (command.getName().equalsIgnoreCase("sic") || command.getName().equalsIgnoreCase("smartitemcreator")) {
+                    if (args.length == 0) {
+                        player.sendMessage("§cUso corretto: /sic <nome_oggetto>");
+                        return false;
+                    }
+                    String itemName = args[0].toLowerCase();
+                    ItemStack item = Items.getItem(itemName);
+                    if (item != null) {
+                        player.getInventory().addItem(item);
+                        player.sendMessage("§aHai ricevuto l'oggetto: " + itemName);
+                    } else {
+                        player.sendMessage("§cOggetto non trovato: " + itemName);
+                    }
+                    return true;
+                }
+            } else {
+                player.sendMessage("Non hai il permesso per questo comando!");
+            }
+        } else {
             if (command.getName().equalsIgnoreCase("sic") || command.getName().equalsIgnoreCase("smartitemcreator")) {
                 if (args.length == 0) {
-                    player.sendMessage("§cUso corretto: /sic <nome_oggetto>");
+                    sender.sendMessage("§cUso corretto: /sic <item> <player>");
+                    return false;
+                } else if (args.length == 1) {
+                    sender.sendMessage("§cGiocatore mancante!");
+                    return false;
+                } else if(args.length !=  2){
                     return false;
                 }
+                Player player = Bukkit.getPlayerExact(args[1]);
                 String itemName = args[0].toLowerCase();
                 ItemStack item = Items.getItem(itemName);
-                if (item != null) {
-                    player.getInventory().addItem(item);
-                    player.sendMessage("§aHai ricevuto l'oggetto: " + itemName);
+                if (player != null) {
+                    if (item != null) {
+                        player.getInventory().addItem(item);
+                        player.sendMessage("§aHai ricevuto l'oggetto: " + itemName);
+                        sender.sendMessage(player.getName() + " ha ricevuto: " + itemName);
+                    } else {
+                        player.sendMessage("§cOggetto non trovato: " + itemName);
+                    }
                 } else {
-                    player.sendMessage("§cOggetto non trovato: " + itemName);
+                    sender.sendMessage("§cGiocatore non trovato!");
                 }
                 return true;
             }
-        } else {
-            player.sendMessage("Non hai il permesso per questo comando!");
         }
         return false;
     }
 
     @Override
     public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        if(command.getName().equalsIgnoreCase("sic") || command.getName().equalsIgnoreCase("smartitemcreator")) {
-            Player player = (Player) sender;
-            if(player.hasPermission("smartitemcreator.cmd")) {
-                List<String> completions = new ArrayList<>();
-                if (args.length == 1) {
-                    for (String itemName : Items.getItemNames()) {
-                        if (itemName.toLowerCase().startsWith(args[0].toLowerCase())) {
-                            completions.add(itemName);
-                        }
+        if (command.getName().equalsIgnoreCase("sic") || command.getName().equalsIgnoreCase("smartitemcreator")) {
+            List<String> completions = new ArrayList<>();
+            if (args.length == 1) {
+                for (String itemName : Items.getItemNames()) {
+                    if (itemName.toLowerCase().startsWith(args[0].toLowerCase())) {
+                        completions.add(itemName);
                     }
                 }
-                return completions;
+            } else if(args.length == 2){
+                completions.addAll(Bukkit.getOnlinePlayers().stream().map(Player::getName)
+                        .filter(p -> p.toLowerCase().startsWith(args[1].toLowerCase()))
+                        .toList());
             }
+            return completions;
         }
         return null;
     }
